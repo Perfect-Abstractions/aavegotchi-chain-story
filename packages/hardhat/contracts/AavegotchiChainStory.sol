2b9254b4dde52fb683e3f23d94f9253082f5c8d0
mudgen <nick@perfectabstractions.com>
2024-08-04 22:49:42 -0400

    Implemented submitting, voting, and publishing story parts

--- packages/hardhat/contracts/AavegotchiChainStory.sol
+++ packages/hardhat/contracts/AavegotchiChainStory.sol
@@ -22,26 +22,35 @@ contract AavegotchiChainStory {
         string authorContact;
         string note;
         string storyPart;
-        uint256 gltrAmount;
-        uint256 vote;
+        uint104 gltrAmount;  //uint104 is 13 bytes
+        uint104 voteScore;
+        uint24 storyPartId;
+        uint16 round;
+        bool published;
     }
 
     struct Author {
-        uint256 disregardedVotingPower;
+        uint104 disregardedVotingPower;
+        bool published;
+        uint24 publishedStoryPartId;
+        uint24[] storyPartIds;
+        mapping(uint256 round => bool) roundsVoted;
     }
 
     struct AppStorage {
         address admin;
         StoryPart[] storyParts;
-        uint16[][] submissions;
-        uint16[] publishedStoryParts;
+        mapping(uint256 round => uint24[] submissions) roundSubmissions;
+        uint24[] publishedStoryParts;
         mapping(address author => Author) authors;
-        uint128 round;
-        uint128 rountStart;
-        uint256 sharedVotingPower;
-        uint256 winningSubmission;
-        uint256 winningSubmissionVote;
-        uint256 gltrMinimum;
+        uint16 totalAuthorsVoted;
+        uint40 submissionStartTime;
+        uint16 round;
+        bool newSubmissionInRound;
+        uint104 sharedVotingPower;
+        uint24 winningSubmissionStoryPartId;        
+        uint104 noSubmissionVoteScore;
+        uint104 gltrMinimum;
     }
 
     AppStorage internal s;
@@ -49,13 +58,148 @@ contract AavegotchiChainStory {
     error NotAdmin(address);
     error ExistingAuthor(address);
     error MissingAuthorInfo(address);
-    error GltrMinimumTooLow(uint256);
-    error GltrBelowMinimum(uint256);
+    error GltrMinimumTooLow(uint256 _newGltrMinimum);
+    error GltrBelowMinimum(uint256 _gltrAmount);
+    error MissingStoryPart();
+    error NotStorySubmissionTimePeriod(uint256 _currentTime);
+    error MaximumSubmissionsReached();
+    error NotVotingTimePeriod(uint256 _currentTime);
+    error CantVoteOnSubmissionFromEarlierRound(address _voter, uint256 _storyPartId);
+    error AlreadyVotedInRound(address _voter, uint256 _round);
+    error NoPowerToVote(address _person);
 
-    constructor() {
-        s.gltrMinimum = 50_000_000;
+    event StoryPartSubmission(uint256 indexed _round, uint256 indexed _storyPartId, address indexed _authorAddress);
+    event StoryPartPublished(uint256 indexed _round, uint256 indexed _storyPartId, address indexed _authorAddress);
+    event SubmissionVote(address indexed _voter, uint256 indexed _round, uint256 indexed _storyPartId);
+
+    constructor(
+        string memory _authorName, 
+        string memory _authorContact, 
+        string memory _note, 
+        string memory _storyPart, 
+        uint256 _gltrAmount
+    ) {
+        if(stringIsEmpty(_authorName) || stringIsEmpty(_authorContact)) {
+            revert MissingAuthorInfo(msg.sender);
+        }
+        s.gltrMinimum = 50_000_000e18;
+        if(_gltrAmount < s.gltrMinimum) {
+            revert GltrBelowMinimum(_gltrAmount);
+        }
+        if(stringIsEmpty(_storyPart)) {
+            revert MissingStoryPart();
+        }
+        uint24 storyPartId = uint24(s.storyParts.length); 
+        s.storyParts.push(
+            StoryPart({
+                authorAddress: msg.sender,
+                authorName: _authorName,
+                authorContact: _authorContact,
+                note: _note,
+                storyPart: _storyPart,
+                gltrAmount: uint104(_gltrAmount),
+                voteScore: 0,
+                storyPartId: storyPartId,
+                round: 0,
+                published: true
+            })
+        );
+        s.sharedVotingPower = uint104(_gltrAmount);
+        s.publishedStoryParts.push(storyPartId);
+        s.submissionStartTime = uint40(block.timestamp);
+        s.round = 1;
+        Author storage author = s.authors[msg.sender];        
+        author.published = true;
+        author.publishedStoryPartId = storyPartId;
+        author.storyPartIds.push(storyPartId);
+        s.winningSubmissionStoryPartId = type(uint24).max;
+
+        emit StoryPartPublished(0, storyPartId, msg.sender);
+    }
+    // Time rules
+    // 1. Seven days to submit story parts.
+    // 2. If one or more stories are submitted during the seven day submission period a voting period starts.
+    // 3. If no stories are sumitted during seven day submission period the seven day submission period restarts.
+    // 4. Voting period lasts seven days unless all authors vote. After voting period ends seven day submission period begins.
+
+    function canSubmitStoryPartWithinSevenDays() internal view returns(bool can_) {
+        return block.timestamp < s.submissionStartTime + 7 days;
     }
 
+    function canSubmitStoryPartAfterSevenDays() internal view returns(bool can_) {
+        return block.timestamp > s.submissionStartTime + 7 days && s.newSubmissionInRound == false;
+    }
+
+    function canSubmitStoryPartAfterFourteenDays() internal view returns(bool can_) {
+        return block.timestamp > s.submissionStartTime + 14;
+    }
+
+    function internalCanSubmitStoryPart() internal view returns(bool can_) {
+        return canSubmitStoryPartWithinSevenDays() || canSubmitStoryPartAfterFourteenDays() || canSubmitStoryPartAfterSevenDays();
+
+    }
+
+    function getCurrentRound() internal view returns(uint256 round_) {
+        round_ = s.round;
+        if(canSubmitStoryPartAfterFourteenDays()) {
+            if(s.newSubmissionInRound) {
+                round_ += ((block.timestamp - (s.submissionStartTime + 14 days)) / 7 days) + 1;  
+            }
+            else {
+                round_ += ((block.timestamp - (s.submissionStartTime + 7 days)) / 7 days) + 1;
+            }
+        }
+        else if(canSubmitStoryPartAfterSevenDays()) {
+            round_++;
+        }
+    }
+
+    function getRoundSubmissionData() 
+        internal view returns(
+            uint256 round_, 
+            uint256 submissionStartTime_) {
+        round_ = s.round;
+        submissionStartTime_ = s.submissionStartTime;        
+        uint256 newRounds = 0;
+        if(canSubmitStoryPartAfterFourteenDays()) {
+            if(s.newSubmissionInRound) {
+                newRounds = (block.timestamp - (submissionStartTime_ + 14 days)) / 7 days;
+                round_ += newRounds;
+                submissionStartTime_ = block.timestamp + (submissionStartTime_ + 14 days) + (newRounds * 7 days);
+            }
+            else {
+                newRounds = (block.timestamp - (submissionStartTime_ + 7 days)) / 7 days;
+                round_ += newRounds;
+                submissionStartTime_ = block.timestamp + (submissionStartTime_ + 7 days) + (newRounds * 7 days);
+            }
+            round_++;
+        }
+        else if(canSubmitStoryPartAfterSevenDays()) {            
+            submissionStartTime_ += 7 days;
+            round_++;
+        }        
+    }
+
+    function getRoundData() 
+        external view returns(
+            uint256 round_, 
+            uint256 submissionStartTime_, 
+            uint256 submissionEndTime_,
+            uint256 voteStartTime_,
+            uint256 voteEndTime_) {
+        (round_, submissionStartTime_) = getRoundSubmissionData();        
+        submissionEndTime_ = submissionStartTime_ + 7 days;
+        if(s.newSubmissionInRound) {
+            voteStartTime_ + submissionEndTime_;
+            voteEndTime_ = voteStartTime_ + 7 days;
+        }
+    }  
+
+
+
+    function canSubmitStoryPart() external view returns(bool can_) {
+        can_ = internalCanSubmitStoryPart();
+    }
     
 
     function submitStoryPart(
@@ -65,7 +209,37 @@ contract AavegotchiChainStory {
         string calldata _storyPart, 
         uint256 _gltrAmount
     ) external {
-        if(s.authors[msg.sender].disregardedVotingPower != 0) {
+        uint256 round;        
+        if(canSubmitStoryPartWithinSevenDays()) {
+            round = s.round;
+        }
+        else {
+            uint256 submissionStartTime;
+            (round, submissionStartTime) = getRoundSubmissionData();
+            if(canSubmitStoryPartAfterFourteenDays()) {
+                if(s.newSubmissionInRound) {
+                    s.totalAuthorsVoted = 0;
+                    uint256 winningStoryPartId = s.winningSubmissionStoryPartId;
+                    StoryPart storage winningStoryPart = s.storyParts[winningStoryPartId];
+                    if(winningStoryPart.voteScore > s.noSubmissionVoteScore) {
+                        winningStoryPart.published = true;
+                        s.publishedStoryParts.push(uint24(winningStoryPartId));
+                        Author storage winningAuthor = s.authors[winningStoryPart.authorAddress];
+                        winningAuthor.published = true;
+                        winningAuthor.publishedStoryPartId = uint24(winningStoryPartId);
+                        emit StoryPartPublished(s.round, winningStoryPartId, winningStoryPart.authorAddress);
+                    }                    
+                    s.noSubmissionVoteScore = 0;
+                    s.winningSubmissionStoryPartId = type(uint24).max;
+                }
+            } else if(canSubmitStoryPartAfterSevenDays() == false) {
+                revert NotStorySubmissionTimePeriod(block.timestamp);
+            }            
+            s.round = uint16(round);
+            s.submissionStartTime = uint40(submissionStartTime);                 
+        }
+        Author storage author = s.authors[msg.sender];        
+        if(author.published) {
             revert ExistingAuthor(msg.sender);
         }
         if(stringIsEmpty(_authorName) || stringIsEmpty(_authorContact)) {
@@ -74,21 +248,122 @@ contract AavegotchiChainStory {
         if(_gltrAmount < s.gltrMinimum) {
             revert GltrBelowMinimum(_gltrAmount);
         }
-        uint256 storyId = s.storyParts.length;        
+        if(stringIsEmpty(_storyPart)) {
+            revert MissingStoryPart();
+        }
+        uint24 storyPartId = uint24(s.storyParts.length);
+        if(storyPartId == type(uint24).max - 1) {  // 16777215 - 1
+            revert MaximumSubmissionsReached();
+        }
         s.storyParts.push(
             StoryPart({
                 authorAddress: msg.sender,
-                authorName: _authorContact,
+                authorName: _authorName,
+                authorContact: _authorContact,
                 note: _note,
                 storyPart: _storyPart,
-                gltrAmount: _gltrAmount,
-                vote: 0
+                gltrAmount: uint104(_gltrAmount),
+                voteScore: 0,
+                storyPartId: storyPartId,
+                round: uint16(round),
+                published: false
             })
-        );
+        );        
+        s.roundSubmissions[round].push(uint24(storyPartId));        
+        author.storyPartIds.push(uint24(storyPartId));
+        s.newSubmissionInRound = true;
 
+        emit StoryPartSubmission(round, storyPartId, msg.sender);
+    }
+     
 
+    function storyPartVote(uint256 _storyPartId) external {
+        if(internalCanSubmitStoryPart()) {
+            revert NotVotingTimePeriod(block.timestamp);
+        }
+        uint256 round = s.round;
+        Author storage author = s.authors[msg.sender];
+        if(author.published == false) {
+            revert NoPowerToVote(msg.sender);
+        }
+        uint256 votingPower = s.sharedVotingPower - author.disregardedVotingPower;
+        if(author.roundsVoted[round]) {
+            revert AlreadyVotedInRound(msg.sender, round);
+        }
+        uint256 totalAuthorsVoted = s.totalAuthorsVoted;
+        if(totalAuthorsVoted == 0) {
+            s.winningSubmissionStoryPartId = uint24(_storyPartId);
+        }        
+        if(_storyPartId != type(uint24).max) { // 16777215
+            StoryPart storage storyPart = s.storyParts[_storyPartId];
+            if(round != storyPart.round) {
+                revert CantVoteOnSubmissionFromEarlierRound(msg.sender, _storyPartId);
+            }
+            uint256 voteScore = storyPart.voteScore + votingPower;
+            storyPart.voteScore = uint104(voteScore);            
+            uint256 winningSubmissionStoryPartId = s.winningSubmissionStoryPartId;
+            if(_storyPartId != winningSubmissionStoryPartId) {                
+                if(voteScore > s.storyParts[winningSubmissionStoryPartId].voteScore) {
+                    s.winningSubmissionStoryPartId = uint24(_storyPartId);
+                }
+            }            
+        }
+        else {
+            s.noSubmissionVoteScore += uint104(votingPower);
+        }
+        emit SubmissionVote(msg.sender, round, _storyPartId);
+        uint256 totalPublishedAuthors = s.publishedStoryParts.length;
+        totalAuthorsVoted++;
+        s.totalAuthorsVoted = uint16(totalAuthorsVoted);
+        if(totalPublishedAuthors == totalAuthorsVoted) {            
+            s.submissionStartTime = uint40(block.timestamp);
+            s.totalAuthorsVoted = 0; 
+            s.newSubmissionInRound = false;
+            uint256 winningStoryPartId = s.winningSubmissionStoryPartId;
+            StoryPart storage winningStoryPart = s.storyParts[winningStoryPartId];
+            if(winningStoryPart.voteScore > s.noSubmissionVoteScore) {
+                winningStoryPart.published = true;
+                s.publishedStoryParts.push(uint24(winningStoryPartId));
+                Author storage winningAuthor = s.authors[winningStoryPart.authorAddress];
+                winningAuthor.published = true;
+                winningAuthor.publishedStoryPartId = uint24(winningStoryPartId);
+                emit StoryPartPublished(s.round, winningStoryPartId, winningStoryPart.authorAddress);
+            }
+            s.round++;
+            s.noSubmissionVoteScore = 0;
+            s.winningSubmissionStoryPartId = type(uint24).max;
+        }
+    }
+    
+
+    function getRoundSubmissionStoryIds(uint256 _round) external view returns(uint24[] memory storyPartIds_) {            
+        storyPartIds_ = s.roundSubmissions[_round];
 
     }
+
+    function getLastSubmissionStoryIds() external view returns(uint24[] memory storyPartIds_) {        
+        uint256 round = getCurrentRound();    
+        storyPartIds_ = s.roundSubmissions[round];
+
+    }
+
+    function getRoundSubmissions(uint256 _round) external view returns(StoryPart[] memory storyParts_) {        
+        uint24[] memory submissions = s.roundSubmissions[_round];
+        storyParts_ = new StoryPart[](submissions.length);
+        for(uint256 i; i < submissions.length; i++) {
+            storyParts_[i] = s.storyParts[submissions[i]];
+        }        
+    }
+
+    function getLastSubmissions() external view returns(StoryPart[] memory storyParts_) {
+        uint256 round = getCurrentRound();
+        uint24[] memory submissions = s.roundSubmissions[round];
+        storyParts_ = new StoryPart[](submissions.length);
+        for(uint256 i; i < submissions.length; i++) {
+            storyParts_[i] = s.storyParts[submissions[i]];
+        }        
+    }
+
 
     function setGltrMinimum(uint256 _newGltrMinimum) external {
         if(msg.sender != s.admin) {
@@ -97,14 +372,17 @@ contract AavegotchiChainStory {
         if(_newGltrMinimum < 10_000) {
             revert GltrMinimumTooLow(_newGltrMinimum);
         }
+        s.gltrMinimum = uint104(_newGltrMinimum);
     }
-    function getGltrMinimum() external returns(uint256) {
+    function getGltrMinimum() external view returns(uint256) {
         return s.gltrMinimum;
     }
 
-    function stringIsEmpty(string calldata _string) internal returns (bool) {
+    function stringIsEmpty(string memory _string) internal pure returns (bool) {
         return 0 == bytes(_string).length;
     }
+
+    
 
     
 
