29c570c40a4f20f8e197a05224cb3eb3547351a8
Jacob Homanics <homanicsjake@gmail.com>
2024-08-07 00:38:25 -0500

    styled contract, minimum, and roundData components

--- packages/nextjs/components/aavegotchi-chain-story/RoundData.tsx
+++ packages/nextjs/components/aavegotchi-chain-story/RoundData.tsx
@@ -1,16 +1,41 @@
 export const RoundData = ({ round }: any) => {
+  const startDate = new Date(Number(round?.[1]) * 1000);
+  const startDateString = startDate.toLocaleString();
+
+  const endDate = new Date(Number(round?.[2]) * 1000);
+  const endDateString = endDate.toLocaleString();
+
+  const voteStartDate = new Date(Number(round?.[3]) * 1000);
+  const voteStartDateString = voteStartDate.toLocaleString();
+
+  const voteEndDate = new Date(Number(round?.[4]) * 1000);
+  const voteEndDateString = voteEndDate.toLocaleString();
+
   return (
-    <div className="flex flex-col bg-secondary">
-      <p className="text-xl">Round Data</p>
-      <p>Round: {round?.[0].toString()}</p>
+    <div className="flex flex-col bg-secondary rounded-lg border-4 border-accent shadow-2xl p-1 text-center">
+      <p className="text-4xl kanit">Round Data</p>
+      <p className="text-2xl kanit">Round</p>
+      <p className="text-xl kanit-light">{round?.[0].toString()}</p>
 
-      <p>Submission Start Time: {round?.[1].toString()}</p>
+      <p className="text-2xl kanit">Submission Start Time </p>
+      <p className="text-xl kanit-light">
+        {startDateString} ({round?.[1].toString()})
+      </p>
 
-      <p>Submission End Time: {round?.[2].toString()}</p>
+      <p className="text-2xl kanit">Submission End Time</p>
+      <p className="text-xl kanit-light">
+        {endDateString} ({round?.[2].toString()})
+      </p>
 
-      <p>Vote Start Time: {round?.[3].toString()}</p>
+      <p className="text-2xl kanit">Vote Start Time</p>
+      <p className="text-xl kanit-light">
+        {voteStartDateString} ({round?.[3].toString()})
+      </p>
 
-      <p>Vote End Time: {round?.[4].toString()}</p>
+      <p className="text-2xl kanit">Vote End Time</p>
+      <p className="text-xl kanit-light">
+        {voteEndDateString} ({round?.[4].toString()})
+      </p>
     </div>
   );
 };
